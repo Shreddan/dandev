e757034588840afe5b7f04a3531385306f47e537
Matt Hayward <matt@matthewhayward.co.uk>
2019-11-09 01:37:31 +0000

    Site Redesign
    - Modernise design and layouts
    - Remove HTML chat

---
+++ WebFings/about-me.php
@@ -0,0 +1,46 @@
+<?php
+    require_once "./partials/header.php";
+    $pageTitle = "About Me";
+    require_once "./partials/title.php"
+?>
+
+<div class="w-3/4 bg-white p-8 shadow-md mx-auto border-teal-700 border-t-4 mb-10">
+    <p class="mb-3">My name is Daniel(Dan) Twitchett. I live in the Republic of Ireland, County Roscommon.</p>
+    <p class="mb-3">I am 28 years old, I have a beautiful partner and 6 wonderful children, two of which are ASD (On the Autism
+        Spectrum), so theres a little background to start us off :)</p>
+    <p class="mb-3">As you may have read on the main page, I started learning to code in around September of 2018, at that time I had
+        rediscovered an old game that I used to play when I was a child, this game was Creatures Exodus(back then it was
+        just called Creatures 3 and Docking Station/Creatures Internet Edition), So I bought the game on GOG.com, and
+        started playing again.</p>
+    <p class="mb-3">However it was a bit fiddly to set up properly, as I had to apply some community made patches in order for it to
+        still work since the original services for this game went down around 10 ish years ago(considering the game is
+        almost 20 years old), if you want to learn about that please go to the
+        <a href="/resources" class="font-bold underline">Resources</a> page to find out more!</p>
+    <p class="mb-3">Whilst on my journey, looking for patches I came across a project known as openc2e on GitHub.</p>
+    <p class="mb-3">The project had been abandoned almost 8 years before, with the only activity really being people updating it to
+        work with the lastest releases of Boost, and from there I decided I wanted to learn how to code</p>
+    <p class="mb-3">Shortly after that in the beginning of 2019, I found the OneLoneCoder Community, after finding Javidx9's youtube
+        channel.</p>
+    <p class="mb-3">I binge-watched most of the videos on the channel, and then found the discord server. I have been a member of
+        this server ever since, and have made many friends.</p>
+    <p class="mb-3">When I first joined the server, I hadnt been coding very long and essentially only knew not even the
+        real basics of coding, I had tried coding in Java before and a little bit of GDScript, but even then I was not
+        very far past the "Hello World" stage. :)</p>
+    <p class="mb-3">With the help of the community and some trial and error by myself(mostly error :P), my confidence is
+        steadily growing.</p>
+    <p class="mb-3">If you have always wanted to code, but dont know where to start or how to go about it, I would highly recommend
+        Javidx9's youtube channel
+        <a href="https://www.youtube.com/channel/UC-yuWVUplUJZvieEligKBkA" class="font-bold underline">OneLoneCoder</a>
+        and I would also suggest joining his discord server.</p>
+    <p>The community is one of the nicest and most helpful that I have ever come across, and Javidx9 is also very active
+        on the server too!</p>
+</div>
+
+<div class="hidden" style="border-style:dotted;">
+	<p style="color:#00000F;font-size:14px;"><b>Have some kind words of encouragement? or perhaps some constructive criticism?<br> leave a comment below tell me what you love/like/hate about this site.<br> Be as brutally honest as you like, you can even comment anonymously if you want :)</b></p>
+    <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...<br></div>
+	<br>
+	<script type="text/javascript" id="hcb"> if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24PVBwcvcToJGz503XA4tts%2F"+"&opts=16862&num=10&ts=1562074373432");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); </script>
+</div>
+
+<?php require_once "./partials/footer.php"; ?>
---
+++ WebFings/resources.php
@@ -0,0 +1,116 @@
+<?php
+    require_once "./partials/header.php";
+    $pageTitle = "Link and Other Useful Things";
+    require_once "./partials/title.php";
+?>
+
+<div class="w-3/4 mx-auto border-teal-700 border-t-4 bg-white shadow-md my-10 p-8">
+    <div class="flex items-center mb-4">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fas fa-fw fa-globe"></i></span></p>
+            <p class="text-lg font-bold">OneLoneCoder</p>
+        </div>
+        <div class="pl-8">
+            Programming Principles and Algorithms and Youtube Videos
+        </div>
+    </div>
+    <div class="flex items-center mb-4">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fas fa-fw fa-globe"></i></span></p>
+            <p class="text-lg font-bold">CppReference</p>
+        </div>
+        <div class="pl-8">
+            C++ library information
+        </div>
+    </div>
+    <div class="flex items-center mb-4">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fas fa-fw fa-globe"></i></span></p>
+            <p class="text-lg font-bold">Top Algorithms</p>
+        </div>
+        <div class="pl-8">
+            Programming algorithms
+        </div>
+    </div>
+    <div class="flex items-center mb-4">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fas fa-fw fa-globe"></i></span></p>
+            <p class="text-lg font-bold">Programiz</p>
+        </div>
+        <div class="pl-8">
+            More on C++ libraries
+        </div>
+    </div>
+    <div class="flex items-center mb-12">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fas fa-fw fa-globe"></i></span></p>
+            <p class="text-lg font-bold">Mad_Doodles</p>
+        </div>
+        <div class="pl-8 w-3/4">
+            This is the Main hub for all things Creatures, it contains Download links to COBs, Metarooms, Bug Fixes, Downlodable Breeds, etc
+        </div>
+    </div>
+    <div class="flex items-center mb-4">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fab fa-fw fa-youtube"></i></span></p>
+            <p class="text-lg font-bold">javidx9</p>
+        </div>
+        <div class="pl-8">
+            Concepts that can be applied to any language (The OneLoneCoder)
+        </div>
+    </div>
+    <div class="flex items-center mb-4">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fab fa-fw fa-youtube"></i></span></p>
+            <p class="text-lg font-bold">TheCodingTrain</p>
+        </div>
+        <div class="pl-8">
+            Coding challenges mainly using javascript
+        </div>
+    </div>
+    <div class="flex items-center mb-12">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fab fa-fw fa-youtube"></i></span></p>
+            <p class="text-lg font-bold">CodeBullet</p>
+        </div>
+        <div class="pl-8">
+            Shows how much fun coding can be. <strong>Warning:</strong> Mature language used...
+        </div>
+    </div>
+    <div class="flex items-center mb-4">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fab fa-fw fa-github-square"></i></span></p>
+            <p class="text-lg font-bold">Shreddan</p>
+        </div>
+        <div class="pl-8">
+            My Github account
+        </div>
+    </div>
+    <div class="flex items-center mb-12">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fab fa-fw fa-github-square"></i></span></p>
+            <p class="text-lg font-bold">OneLoneCoder</p>
+        </div>
+        <div class="pl-8">
+            OneLoneCoder Github account
+        </div>
+    </div>
+    <div class="flex items-center mb-4">
+        <div class="flex items-center w-1/4">
+            <p><span class="text-4xl text-teal-800 mr-4"><i class="fab fa-fw fa-discord"></i></span></p>
+            <p class="text-lg font-bold">Coding Cavern</p>
+        </div>
+        <div class="pl-8">
+            My server - coding, gaming, general chit-chat - Come on in!!!
+        </div>
+    </div>
+</div>
+
+<div class="hidden" style="border-style:dotted;">
+<p style="color:#00000F;font-size:14px;"><b>Have some kind words of encouragement? or perhaps some constructive criticism?<br> leave a comment below tell me what you love/like/hate about this site.<br> Be as brutally honest as you like, you can even comment anonymously if you want :)</b></p>
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...<br></div>
+ <br>
+ <script type="text/javascript" id="hcb"> if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24PVBwcvcToJGz503XA4tts%2F"+"&opts=16862&num=10&ts=1562074373432");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); </script>
+ </div>
+
+<?php require_once "./partials/footer.php"; ?>
